977b1b93f89df3763ee0c62156cc6deb5a2891d9
kevin86wright <kevin86wright@gmail.com>
2021-03-15 21:01:43 +0000

    Add indexing to table migration, add readme images, gitignore ip repo

--- table_migrate.sql
+++ table_migrate.sql
@@ -6,6 +6,12 @@ CREATE TABLE IF NOT EXISTS ip_project.networks LIKE ip_project.networks_tmp;
 DROP TABLE IF EXISTS ip_project.addresses_old;
 DROP TABLE IF EXISTS ip_project.networks_old;
 
+-- Add table indexes
+ALTER TABLE ip_project.addresses_tmp ADD INDEX `address` (`address`);
+ALTER TABLE ip_project.networks_tmp ADD INDEX `nw_start` (`nw_start`);
+ALTER TABLE ip_project.networks_tmp ADD INDEX `nw_end` (`nw_end`);
+
+
 -- Perform atomic table swap operation
 RENAME TABLE ip_project.addresses TO ip_project.addresses_old, ip_project.addresses_tmp To ip_project.addresses;
 RENAME TABLE ip_project.networks TO ip_project.networks_old, ip_project.networks_tmp To ip_project.networks;
@@ -13,3 +19,4 @@ RENAME TABLE ip_project.networks TO ip_project.networks_old, ip_project.networks
 --  Drop old tables
 DROP TABLE ip_project.addresses_old;
 DROP TABLE ip_project.networks_old;
+
